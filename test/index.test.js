676325f9c690979be1398dc4df74f7ea1d86201b
Susanna Martinelli <susanna.martinelli@buongiorno.com>
2017-02-09 17:57:19 +0100

    adding test

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,13 @@
 'use strict'
-var test = require('tape');
+const test = require('tap');
+const levelup = require('levelup')
+const assertService = require('../index')
+
+const filepath = './tmp/mydb'
+let db = levelup(filepath)
+let assert =  { name : 'my_new_assert' }
+assertService(filepath).insertAssert(assert, function(err, id){
+  db.get(id, function(err, value){
+    tap.deepEqual(value, assert)
+  })
+})
