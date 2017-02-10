3c95615ae7a44f9d20ae6350fedf4a35b99175cb
Susanna Martinelli <susanna.martinelli@buongiorno.com>
2017-02-10 11:12:41 +0100

    adding test

--- index.js
+++ index.js
@@ -4,73 +4,68 @@ const levelup = require('levelup');
 const Joi = require('joi');
 const uuidV1 = require('uuid/v1');
 const options = {
-    createIfMIssing: false,
+    createIfMIssing: true,
     valueEncoding: 'json'
 }
-let db;
 
 var assetSchemaInsert = Joi.object().keys({
     name: Joi.string().alphanum().min(3).max(30).required()
-}).with('name');
+})
 
 
 var assetSchemaUpdate = Joi.object().keys({
     id: Joi.string().guid({ version: ['uuidv1']}).required(),
     status: Joi.string().alphanum().min(3).max(30).required()
-}).with('id','status')
+})
 
 var assetSchemaQuery = Joi.object().keys({
     id: Joi.string().guid({ version: ['uuidv1']}).required()
-}).with('id')
-
-function insert(object, cb) {
-    Joi.validate(object, assetSchemaInsert, function (err, value) {
-        if (err) cb(err)
-        let id = uuidV1();
-        db.put(id, object, function(err) {
-             if (err) cb(err)
-             cb(id)
-        })
-     });  
-    
-}
-
-function update() {
-    Joi.validate(object, assetSchemaUpdate, function (err, value) {
-        if (err) cb(err)
-
-        db.get(object.id, function(err, value) {
-            if (err) cb(err)
-            db.put(id, value, function(err) {
-                if (err) cb(err)
-                cb();
-            })
-        })
-    
-
-     });  
-    
-}
-
-function query(object) {
-    Joi.validate(object, assetSchemaQuery, function (err, value) {
-        if (err) cb(err)
-        db.get(object.id, function(err, value) {
-            if (err) cb(err)
-            cb(value);
-        })
-     });  
-}
+})
 
 
-module.exports = function(filePath) {
 
-
-    db = levelup(filePath, options);    
-
+module.exports = function(db) {
     return {
-        insert: insert,
-        update: update,
-        query: query
+        insert,
+        update,
+        query
     };
+
+
+    function insert(object, cb) {
+        Joi.validate(object, assetSchemaInsert, function (err, value) {
+            if (err) return cb(err)
+            let id = uuidV1();
+            db.put(id, object, function(err) {
+                 if (err) return cb(err)
+                 cb(null,id)
+            })
+         });
+
+    }
+
+    function update(object, cb) {
+        Joi.validate(object, assetSchemaUpdate, function (err, value) {
+            if (err) return cb(err)
+
+            db.get(object.id, function(err, value) {
+                if (err) return cb(err)
+                value.status = object.status
+                db.put(object.id, value, function(err) {
+                    if (err) return cb(err)
+                    cb()
+                })
+            })
+          })
+    }
+
+    function query(object) {
+        Joi.validate(object, assetSchemaQuery, function (err, value) {
+            if (err) cb(err)
+            db.get(object.id, function(err, value) {
+                if (err) return cb(err)
+                cb(null, value)
+            })
+         });
+    }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,51 @@
 'use strict'
-const test = require('tap');
+const tap = require('tap');
 const levelup = require('levelup')
 const assertService = require('../index')
+const Memdb = require('memdb')
+const options = {
+    createIfMIssing: true,
+    valueEncoding: 'json'
+}
+let db = Memdb(options)
 
-const filepath = './tmp/mydb'
-let db = levelup(filepath)
-let assert =  { name : 'my_new_assert' }
-assertService(filepath).insertAssert(assert, function(err, id){
-  db.get(id, function(err, value){
-    tap.deepEqual(value, assert)
+
+
+
+tap.test('test wrong param in Insert', t => {
+  let assert = {nome:'ilnome'}
+  assertService(db).insert(assert, function(err, id){
+    t.ok(err)
+    t.end()
+  })
+})
+
+
+
+tap.test('test Insert ok' , t => {
+  let assert = {name:'ilmioassert'}
+  assertService(db).insert(assert, function(err, id){
+    t.notOk(err)
+    t.ok(id)
+    t.end()
+  })
+})
+
+tap.test('test wrong param in Update' , t => {
+  let assert = { id :'ilmioassert' }
+  assertService(db).update(assert, function(err, id){
+    t.ok(err)
+    t.end()
+  })
+})
+
+tap.test('test update ok' , t => {
+  let assert = { name:'ilmioassert' }
+  assertService(db).insert(assert, function(err, id) {
+    t.notOk(err)
+    assertService(db).update({ id: id , status: 'updated' }, function(err){
+      t.notOk(err)
+      t.end()
+    })
   })
 })
